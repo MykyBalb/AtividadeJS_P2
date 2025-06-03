c44245c02531407ce7a242588d32683ece7578bb
MykyBalb <mykaelbalbino06@gmail.com>
2025-06-03 15:40:54 -0300

    Ex06

---
+++ Ex06/js/script.js
@@ -0,0 +1,46 @@
+class ContadorCaracter {
+    constructor(texto) {
+        this.texto = texto.trim();
+    }
+
+    contarcaracter() {
+        if (this.texto === '') {
+            return 0;
+        }
+        const caracter = this.texto.split('');
+        return caracter.length;
+    }
+}
+
+const textoInput = document.getElementById('textoInput');
+const btnContarcaracter = document.getElementById('btnContarCaracter');
+const resultadoParagrafo = document.getElementById('resultado');
+const errorMessage = document.getElementById('errorMessage');
+
+function showErrorMessage(message) {
+    errorMessage.textContent = message;
+    errorMessage.style.display = 'block';
+    setTimeout(() => {
+        errorMessage.style.display = 'none';
+    }, 3000);
+}
+
+btnContarcaracter.addEventListener('click', () => {
+    const textoDigitado = textoInput.value;
+
+    if (textoDigitado.trim() === '') {
+        showErrorMessage('Por favor, digite algum texto para contar as caracter.');
+        resultadoParagrafo.textContent = 'Número de caracter: 0';
+        return;
+    }
+
+    const meuContador = new ContadorCaracter(textoDigitado);
+
+    const numcaracter = meuContador.contarcaracter();
+    resultadoParagrafo.textContent = `Número de caracter: ${numcaracter}`;
+});
+
+textoInput.addEventListener('input', () => {
+    resultadoParagrafo.textContent = 'Número de caracter: 0'; 
+    errorMessage.style.display = 'none'; 
+});
